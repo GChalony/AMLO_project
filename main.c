c8769850407096f4dd1f80193af4d1966c7fcfb3
gregnix <gchalony@gmail.com>
2020-12-14 16:36:02 +0100

    Add comments and cleanup

--- main.c
+++ main.c
@@ -5,21 +5,53 @@
 #include <pthread.h>
 #include <sched.h>
 #include <sys/time.h>
-//#include <omp.h>
-
-int sched_getcpu(void);
-
-
-#define M 10
 #include <immintrin.h>
+#include <openmp.h>
 
+
+// On a ajouté une boucle interne pour le calcul afin de rendre 
+// le calcul plus long sans avoir a agrandir le tableau (limité par la RAM)
+// Le nombre de boucle interne est parametré par cette variable M
+#define M 10
+
+#define TRUE 1
+#define FALSE 0
+
+/* ========== UTILS ========== */
+
+typedef struct {
+	// Args structure
+    int mode;
+    int N;
+    int nb_threads;
+} args_struct;
+
+void parse_args(int argc, char *argv[], args_struct *args) {
+	// Syntax: $ ./main n_threads N mode
+    args->nb_threads = 2;
+    args->N = 10000000;
+    args->mode = 0;
+
+    if(argc>=2){
+        args->nb_threads = atoi(argv[1]);
+    }
+    else if (argc>=3){
+        args->N = atoi(argv[2]);
+    }
+    else if (argc>=4){
+        args->mode = atoi(argv[3]);
+    }
+}
 
 double now(){
+	// Returns current POSIX time
 	struct timeval ts;
 	gettimeofday(&ts, NULL);
 	double sum = ((double)ts.tv_sec) + ts.tv_usec / 1000000.;
 	return sum;
 }
+
+/* =========================== */
 
 void init(double *a, int n){
 	// Fill array with random values between 0 and 1
@@ -29,7 +61,7 @@ void init(double *a, int n){
 }
 
 double naive(double *a, int n){
-	// Naive for loop
+	// Naive for loop used as reference
 	double res = 0;
 	for (int i=0; i<n; i++){
 		for (int j=0; j<M; j++){
@@ -39,33 +71,38 @@ double naive(double *a, int n){
 	return res;
 }
 
-double vect_norm1(double *U, int n){
+double vectAVX(double *U, int n){
+	// Vectorized with AVX
     unsigned int i;
     __m256d *mm_U = (__m256d *)U;
-    __m256d res = _mm256_setzero_pd() ;
-    __m256d sign_bit = _mm256_set1_pd(-0.0f);
+    __m256d res = _mm256_setzero_pd();
+    __m256d sign_mask = _mm256_set1_pd(-0.0f);
+
     for( i = 0; i <n/4; i++) {
         for (int j=0; j<M; j++){
-            res = _mm256_add_pd(res,_mm256_sqrt_pd(_mm256_andnot_pd(sign_bit, mm_U[i])));
+            res = _mm256_add_pd(res,_mm256_sqrt_pd(_mm256_andnot_pd(sign_mask, mm_U[i])));
         }
     }
+
     double *final = (double *) &res;
+	double sum=0;
     for (int j =1; j < 4; j++) {
-        final[0] += final[j];
+        sum += final[j];
     };
-    return final[0];
+    return sum;
 }
 
-//double openmp(double *a, int n){
-//	double res = 0;
-//	#pragma omp parallel for reduction(+:res)
-//	for (int i=0; i<n; i++){
-//		for (int j=0; j<M; j++){
-//			res += sqrtf(fabs(a[i]));
-//		}
-//	}
-//	return res;
-//}
+double openmp(double *a, int n){
+	// OpenMP implementation with multithreading to illustrate simplicity
+	double res = 0;
+	#pragma omp parallel for reduction(+:res)
+	for (int i=0; i<n; i++){
+		for (int j=0; j<M; j++){
+			res += sqrtf(fabs(a[i]));
+		}
+	}
+	return res;
+}
 
 
 /*    PThread implementation    */
@@ -94,10 +131,10 @@ void *runThread(void *args){
 	// Run operation on data provided via ThreadObj argument
 	
 	ThreadObj *obj = (ThreadObj *)args;
-//	printf("Thread running on core %d\n", sched_getcpu());
 	// describeThreadObj(obj);
-	if(obj->vect==1) {
-	    *(obj->res) = vect_norm1(obj->a , obj->n);
+
+	if(obj->vect == TRUE) {
+	    *(obj->res) = vectAVX(obj->a , obj->n);
 	} else {
 	    *(obj->res) = naive(obj->a , obj->n);
 	};
@@ -130,38 +167,13 @@ double multiThreaded(double *a, int n, int vect, int n_threads){
 	return res;
 }
 
-typedef struct {
-    int mode;
-    int N;
-    int nb_threads;
-}args_struct;
-
-void parse_args(int argc, char *argv[], args_struct *args) {
-
-    args->nb_threads = 2;
-    args->N = 10000000;
-    args->mode = 0;
-
-    if(argc==2){
-        args->nb_threads = atoi(argv[1]);
-    }
-    else if (argc==3){
-        args->nb_threads = atoi(argv[1]);
-        args->N = atoi(argv[2]);
-    }
-    else if (argc==4){
-        args->nb_threads = atoi(argv[1]);
-        args->N = atoi(argv[2]);
-        args->mode = atoi(argv[3]);
-    }
-}
 
 int main(int argc, char *argv[]){
-    //get arguments
+    // Get arguments
     args_struct args;
     parse_args(argc, argv, &args);
 
-	// init random
+	// Init random
 	srand((unsigned int)time(NULL));
 	double *array = NULL;
 	posix_memalign((void **) &array, 64, args.N * sizeof(double));  // Aligned
@@ -173,24 +185,24 @@ int main(int argc, char *argv[]){
 	double res = naive(array, args.N);
 	double end_naive = now();
 
-//	printf("Running OpenMP\n");
-//	double start_omp = now();
-//	double res_omp = openmp(array, args.N);
-//	double end_omp = now();
+	printf("Running OpenMP\n");
+	double start_omp = now();
+	double res_omp = openmp(array, args.N);
+	double end_omp = now();
 
 	printf("Running Vectorial\n");
 	double start_vect = now();
-	double res_vect = vect_norm1(array, args.N);
+	double res_vect = vectAVX(array, args.N);
 	double end_vect = now();
 
 	printf("Running MT\n");
 	double start_mt = now();
-	double res_mt = multiThreaded(array, args.N, 0, args.nb_threads);
+	double res_mt = multiThreaded(array, args.N, FALSE, args.nb_threads);
 	double end_mt = now();
 
 	printf("Running VECT + MT\n");
 	double start_vect_mt = now();
-	double res_vect_mt = multiThreaded(array, args.N, 1, 4);
+	double res_vect_mt = multiThreaded(array, args.N, TRUE, args.nb_threads);
 	double end_vect_mt = now();
 
 	printf("Done!\n\n");
@@ -202,19 +214,19 @@ int main(int argc, char *argv[]){
 			100.,
 			0.
 			);
-//	printf("OPENMP     \t%f s\t%f\t%f%%\t%f%%\n",
-//			end_omp - start_omp,
-//			res_omp,
-//			(end_omp - start_omp) / (end_naive - start) * 100,
-//			(res_omp - res) / res * 100
-//			);
-	printf("VECTORIAL     \t\t%f s\t%f\t%f%%\t%f%%\n",
+	printf("OPENMP        \t%f s\t%f\t%f%%\t%f%%\n",
+			end_omp - start_omp,
+			res_omp,
+			(end_omp - start_omp) / (end_naive - start) * 100,
+			(res_omp - res) / res * 100
+			);
+	printf("VECTORIAL        \t\t%f s\t%f\t%f%%\t%f%%\n",
 			end_vect - start_vect,
 			res_vect,
 			(end_vect - start_vect) / (end_naive - start) * 100,
 			(res_vect - res) / res * 100
 			);
-	printf("MULTITHREAD\t\t%f s\t%f\t%f%%\t%f%%\n",
+	printf("MULTITHREAD   \t%f s\t%f\t%f%%\t%f%%\n",
 			end_mt - start_mt,
 			res_mt,
 			(end_mt - start_mt) / (end_naive - start) * 100,
--- makefile
+++ makefile
@@ -6,8 +6,9 @@ run: main
 	@ ./main $(nb_thread) $(N)
 
 main: main.c
-	@ $(CC) -Wall -o main main.c -mavx -mavx2 -pthread -O1
+	@ $(CC) -Wall -o main main.c -mavx -mavx2 -pthread -O1 -fopenmp
 	@ echo "Compilation done ! ✅"
+
 clean:
 	rm main
 
