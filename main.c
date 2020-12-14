e4339fa64748d8d4a4775b12fb471a8133608a5a
gregnix <gchalony@gmail.com>
2020-12-14 01:08:13 +0100

    Double version of MT

--- main.c
+++ main.c
@@ -3,19 +3,20 @@
 #include <math.h>
 #include <time.h>
 #include <pthread.h>
+#include <sched.h>
 
 #define N 100000000
 
 
-void init(float *a, int n){
+void init(double *a, int n){
 	for (int i=0; i<n; i++){
-		a[i] = ((float)rand()/(float)(RAND_MAX));
+		a[i] = ((double)rand()/(double)(RAND_MAX)) / n;
 	}
 }
 
 
-float naive(float *a, int n){
-	float res = 0;
+double naive(double *a, int n){
+	double res = 0;
 	for (int i=0; i<n; i++){
 		res += sqrtf(fabsf(a[i]));
 	}
@@ -23,13 +24,13 @@ float naive(float *a, int n){
 }
 
 typedef struct {
-	float *a;
+	double *a;
 	int n;
 	int t;
-	float *res;
+	double *res;
 } ThreadObj;
 
-void instantiateThreadObj(ThreadObj *obj, float *a, int n, int t, float *res){
+void instantiateThreadObj(ThreadObj *obj, double *a, int n, int t, double *res){
 	obj->a = a;
 	obj->n = n;
 	obj->t = t;
@@ -37,16 +38,19 @@ void instantiateThreadObj(ThreadObj *obj, float *a, int n, int t, float *res){
 }
 
 void describeThreadObj(ThreadObj *obj){
-	printf("ThreadObj: n=%d, t=%d, res=%f address of a=%d", obj->n, obj->t, *(obj->res), &(obj->a));
+	printf("ThreadObj: n=%d, t=%d, res=%f address of a=%d\n", obj->n, obj->t, *(obj->res), &(obj->a));
 }
 
 void *runThread(void *args){
 	ThreadObj *obj = (ThreadObj *)args;
-	printf("\nIn thread: ");
+	printf("\nIn thread on core %d: ", sched_getcpu());
 	describeThreadObj(obj);
-	float res = 0;
+	double res = 0;
 
 	for (int i=0; i<obj->n; i++){
+		// if (i % 100000 == 0){
+		// 	printf("\tFrom thread %d at iteration %d\n", obj->t, i);
+		// }
 		res += sqrtf(fabsf(obj->a[i]));
 	}
 
@@ -55,10 +59,10 @@ void *runThread(void *args){
 	return NULL;
 }
 
-float multiThreaded(float *a, int n, int n_threads){
+double multiThreaded(double *a, int n, int n_threads){
 	pthread_t *th_id = malloc(n_threads * sizeof(pthread_t));
 	
-	float *resultsPerThread = malloc(n_threads * sizeof(float));
+	double *resultsPerThread = malloc(n_threads * sizeof(double));
 	int offset=0, m = n / n_threads;  // assumes n multiple of n_threads
 	
 	for (int t=0; t<n_threads; t++){
@@ -74,7 +78,7 @@ float multiThreaded(float *a, int n, int n_threads){
 	
 	printf("\n");
 
-	float res = 0;
+	double res = 0;
 	for (int i=0; i<n_threads; i++){
 		pthread_join(th_id[i], NULL);
 		res += resultsPerThread[i];
@@ -87,31 +91,33 @@ float multiThreaded(float *a, int n, int n_threads){
 int main(){
 	// init random
 	srand((unsigned int)time(NULL));
-	float *array = malloc(N * sizeof(float));
-	// float array[N] __attribute__((aligned(64)));
+	double *array = malloc(N * sizeof(double));
+	// double array[N] __attribute__((aligned(64)));
 
 	init(array, N);
 
-	clock_t start = clock();
+	double start = clock();
 
-	float res = naive(array, N);
+	double res = naive(array, N);
 	
-	clock_t end_naive = clock();
+	double end_naive = clock();
 
-	clock_t start_mt = clock();
+	double start_mt = clock();
 
-	float res_mt = multiThreaded(array, N, 4);
+	double res_mt = multiThreaded(array, N, 4);
 
-	clock_t end_mt = clock();
-	printf("Array of size %d\nTook %f seconds\nResult = %f\n", 
+	double end_mt = clock();
+	printf("NAIVE : array of size %d\nTook %f seconds\nResult = %f\n", 
 			N, 
 			(end_naive - start)/(double)CLOCKS_PER_SEC, 
 			res 
 			);
-	printf("With multithreaded took %f seconds\nResult = %f\n",
+	printf("MULTITHREAD : With multithreaded took %f seconds\nResult = %f\n",
 			(end_mt - start_mt)/(double)CLOCKS_PER_SEC,
 			res_mt
 			);
 
+
+	printf("Comparison:\n Error of %f%%\n Time ratio %f%%\n", (res - res_mt) / res * 100, (end_mt - start_mt) / (end_naive - start) * 100);
 	return 0;
 }
