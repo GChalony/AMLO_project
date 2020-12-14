c19cbf8a51d861bf1f1c7c2e16f2971433de2785
gregnix <gchalony@gmail.com>
2020-12-14 09:57:40 +0100

    Correct multithreaded version with pthread and OpenMP

--- main.c
+++ main.c
@@ -4,9 +4,21 @@
 #include <time.h>
 #include <pthread.h>
 #include <sched.h>
+#include <sys/time.h>
+#include <omp.h>
+
+int sched_getcpu(void);
 
 #define N 100000000
+#define M 20
 
+
+double now(){
+	struct timeval ts;
+	gettimeofday(&ts, NULL);
+	double sum = ((double)ts.tv_sec) + ts.tv_usec / 1000000.;
+	return sum;
+}
 
 void init(double *a, int n){
 	for (int i=0; i<n; i++){
@@ -14,23 +26,36 @@ void init(double *a, int n){
 	}
 }
 
-
 double naive(double *a, int n){
 	double res = 0;
 	for (int i=0; i<n; i++){
-		res += sqrtf(fabsf(a[i]));
+		for (int j=0; j<M; j++){
+			res += sqrtf(fabsf(a[i]));
+		}
+	}
+	return res;
+}
+
+double openmp(double *a, int n){
+	double res = 0;
+	#pragma omp parallel for reduction(+:res)
+	for (int i=0; i<n; i++){
+		for (int j=0; j<M; j++){
+			res += sqrtf(fabsf(a[i]));
+		}
 	}
 	return res;
 }
 
 typedef struct {
-	double *a;
-	int n;
-	int t;
-	double *res;
+	double *a;  	// Array to compute operation on
+	int n;  		// Number of items in array
+	int t;  		// Thread ID
+	double *res;	// Location to store result
 } ThreadObj;
 
 void instantiateThreadObj(ThreadObj *obj, double *a, int n, int t, double *res){
+	// Utils function to fill ThreadObj attributes
 	obj->a = a;
 	obj->n = n;
 	obj->t = t;
@@ -38,23 +63,23 @@ void instantiateThreadObj(ThreadObj *obj, double *a, int n, int t, double *res){
 }
 
 void describeThreadObj(ThreadObj *obj){
-	printf("ThreadObj: n=%d, t=%d, res=%f address of a=%d\n", obj->n, obj->t, *(obj->res), &(obj->a));
+	printf("ThreadObj: n=%d, t=%d, res=%f address of a=%p\n", obj->n, obj->t, *(obj->res), &(obj->a));
 }
 
 void *runThread(void *args){
+	// Run operation on data provided via ThreadObj argument
+	
 	ThreadObj *obj = (ThreadObj *)args;
-	printf("\nIn thread on core %d: ", sched_getcpu());
-	describeThreadObj(obj);
+	printf("Thread running on core %d", sched_getcpu());
+	// describeThreadObj(obj);
 	double res = 0;
 
 	for (int i=0; i<obj->n; i++){
-		// if (i % 100000 == 0){
-		// 	printf("\tFrom thread %d at iteration %d\n", obj->t, i);
-		// }
-		res += sqrtf(fabsf(obj->a[i]));
+		for (int j=0; j<M; j++){
+			res += sqrtf(fabsf(obj->a[i]));
+		}
 	}
 
-	printf("Thread %d got res=%f\n", obj->t, res);
 	*(obj->res) = res;
 	return NULL;
 }
@@ -71,9 +96,7 @@ double multiThreaded(double *a, int n, int n_threads){
 		ThreadObj *obj = malloc(sizeof(ThreadObj));
 		instantiateThreadObj(obj, a + offset, m, t, &resultsPerThread[t]);
 		
-		printf("\nCreated thread: ");
-		describeThreadObj(obj);
-		pthread_create(&th_id[t], NULL, *runThread, obj);
+		pthread_create(&th_id[t], NULL, runThread, obj);
 	}
 	
 	printf("\n");
@@ -82,7 +105,6 @@ double multiThreaded(double *a, int n, int n_threads){
 	for (int i=0; i<n_threads; i++){
 		pthread_join(th_id[i], NULL);
 		res += resultsPerThread[i];
-		printf("Thread %d computed %f\n", i, resultsPerThread[i]);
 	}
 
 	return res;
@@ -96,28 +118,42 @@ int main(){
 
 	init(array, N);
 
-	double start = clock();
-
+	printf("Running naive\n");
+	double start = now();
 	double res = naive(array, N);
-	
-	double end_naive = clock();
+	double end_naive = now();
 
-	double start_mt = clock();
+	printf("Running OpenMP\n");
+	double start_omp = now();
+	double res_omp = openmp(array, N);
+	double end_omp = now();
 
+	printf("Running MT\n");
+	double start_mt = now();
 	double res_mt = multiThreaded(array, N, 4);
+	double end_mt = now();
 
-	double end_mt = clock();
-	printf("NAIVE : array of size %d\nTook %f seconds\nResult = %f\n", 
-			N, 
-			(end_naive - start)/(double)CLOCKS_PER_SEC, 
-			res 
+	printf("Done!\n");
+
+	printf("METHOD     \tTIME     \tRESULT     \tTime ratio\tError ratio\n");
+	printf("NAIVE     \t%f s\t%f\t%f%%\t%f%%\n", 
+			end_naive - start, 
+			res,
+			100.,
+			0.
 			);
-	printf("MULTITHREAD : With multithreaded took %f seconds\nResult = %f\n",
-			(end_mt - start_mt)/(double)CLOCKS_PER_SEC,
-			res_mt
+	printf("OPENMP     \t%f s\t%f\t%f%%\t%f%%\n", 
+			end_omp - start_omp, 
+			res_omp,
+			(end_omp - start_omp) / (end_naive - start) * 100,
+			(res_omp - res) / res * 100
+			);
+	printf("MULTITHREAD\t%f s\t%f\t%f%%\t%f%%\n", 
+			end_mt - start_mt,
+			res_mt,
+			(end_mt - start_mt) / (end_naive - start) * 100,
+			(res_mt - res) / res * 100
 			);
 
-
-	printf("Comparison:\n Error of %f%%\n Time ratio %f%%\n", (res - res_mt) / res * 100, (end_mt - start_mt) / (end_naive - start) * 100);
 	return 0;
 }
